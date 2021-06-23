97e26ca4c0c09509c436b276a03a84baa2d0a5b7
skidipap <mad.skidipap@gmail.com>
2021-06-23 18:58:06 +0700

    base

---
+++ src/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
+from config.db import DB_CONFIG
+from config.settings import Settings
+import uvicorn
+
+settings = Settings()
+app = FastAPI(title=settings.APP_NAME)
+
+register_tortoise(
+    app,
+    config=DB_CONFIG,
+    generate_schemas=False,
+)
+
+w
+if __name__ == '__main__':
+    uvicorn.run(app, host=settings.HOST, port=settings.PORT)
